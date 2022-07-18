e3159eb0c2bfb0f0bd2ef7179e7ec45f982fb777
Bert Palm <bert.palm@ufz.de>
2022-07-18 17:58:59 +0200

    removed multiple flag from mqtt_[broker,user,password], refactored some `format` in favor of f-strings

--- src/AbstractAction.py
+++ src/AbstractAction.py
@@ -14,14 +14,14 @@ class AbstractAction(ABC):
         self.mqtt_user = mqtt_user
         self.mqtt_password = mqtt_password
 
-        self.mqtt_host = self.mqtt_broker[0].split(":")[0]
-        self.mqtt_port = int(self.mqtt_broker[0].split(":")[1])
+        self.mqtt_host = self.mqtt_broker.split(":")[0]
+        self.mqtt_port = int(self.mqtt_broker.split(":")[1])
         self.mqtt_client = mqtt.Client()
 
     def connect_mqtt(self):
         self.mqtt_client.user_data_set({"schema_file": self.SCHEMA_FILE,
                                         "act": self.act,
-                                        "mqtt_broker": self.mqtt_broker[0]})
+                                        "mqtt_broker": self.mqtt_broker})
         self.mqtt_client.username_pw_set(self.mqtt_user, self.mqtt_password)
         self.mqtt_client.on_connect = on_connect
         self.mqtt_client.on_log = on_log
