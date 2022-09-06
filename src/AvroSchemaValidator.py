bd2818f416f41b925c6aeb04c80d38d56f33a118
Bert Palm <bert.palm@ufz.de>
2022-09-06 18:06:30 +0200

    improved logging usage

--- src/AvroSchemaValidator.py
+++ src/AvroSchemaValidator.py
@@ -9,7 +9,7 @@ def validate_avro_schema(message, schema_file):
         schema = load_schema(schema_file)
         try:
             validate(message, schema)
-            logging.info("Received message matches schema!")
+            logging.debug("Received message matches schema!")
             return True
         except ValidationError:
             logging.warning("Received message does not match given avro schema!")
