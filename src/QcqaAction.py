505731d83ef09ff0d27de7c66be078451c2f3a7a
Bert Palm <bert.palm@ufz.de>
2022-10-24 15:01:30 +0200

    refactoring, added support for context_window be a numeric or datetime-offset window

--- src/QcqaAction.py
+++ src/QcqaAction.py
@@ -3,6 +3,8 @@
 
 from __future__ import annotations
 
+import datetime
+
 from AbstractAction import AbstractAction
 from tsm_datastore_lib import SqlAlchemyDatastore
 from tsm_datastore_lib.SqlAlchemy.Model import Observation, Thing, Datastream
@@ -59,56 +61,127 @@ class QcqaAction(AbstractAction):
             )
         return stream
 
-    def get_data(
-            self,
-            datastore: SqlAlchemyDatastore,
-            datastream: Datastream,
-            window,
-            columns=None,
-    ) -> pd.DataFrame:
+    def parse_window(self, window) -> datetime.timedelta | int:
+        # todo: 4 test for (+/-window X offset/numeric)
+        if isinstance(window, int) or isinstance(window, str) and window.isnumeric():
+            window = int(window)
+            is_negative = window < 0
+        else:
+            window = pd.Timedelta(window).to_pydatetime()
+            is_negative = window.days < 0
+        if is_negative:
+            raise ValueError("window can't be negative")
+        return window
 
-        obs = datastore.session.query(Observation.result_time).filter(
+    def get_data(
+        self,
+        datastore: SqlAlchemyDatastore,
+        datastream: Datastream,
+        window: int | datetime.timedelta,
+    ) -> pd.DataFrame | None:
+
+        query = datastore.session.query(Observation.result_time).filter(
             Observation.datastream == datastream,
             Observation.result_quality == sqlalchemy.JSON.NULL,
         )
-        more_recent = obs.order_by(sqlalchemy.desc(Observation.result_time)).first()
-        less_recent = obs.order_by(sqlalchemy.asc(Observation.result_time)).first()
+        more_recent = query.order_by(sqlalchemy.desc(Observation.result_time)).first()
+        less_recent = query.order_by(sqlalchemy.asc(Observation.result_time)).first()
 
         # no new observations
         if more_recent is None:
-            return pd.DataFrame()
+            # todo: throw warning ?
+            return None
+        else:
+            more_recent = more_recent[0]
+            less_recent = less_recent[0]
 
-        # todo: numeric window many entries before less_recent
-        # todo: datetime size window before less_recent
-        sql = datastore.session.query(
-            Observation.result_time, sqlalchemy.text("result_number")
-        ).filter(
-            Observation.datastream == datastream,
-            Observation.result_time <= more_recent[0],
-            Observation.result_time >= less_recent[0],
+        base_query = datastore.session.query(Observation).filter(
+            Observation.datastream == datastream
         )
 
-        return pd.read_sql(
-            sql.statement, sql.session.bind, parse_dates=True, index_col="result_time"
+        # get all new (unprocessed) data
+        query = base_query.filter(
+            Observation.result_time <= more_recent,
+            Observation.result_time >= less_recent,
         )
+        main_data = pd.read_sql(
+            query.statement,
+            query.session.bind,
+            parse_dates=True,
+            index_col="result_time",
+        )
+
+        if main_data.empty:
+            return None
+
+        # numeric window
+        # --------------
+        # get window-many additional observations
+        # preceding the first observation of the new data.
+        # the window define a number of observations e.g. `500`
+        if isinstance(window, int):
+
+            query = base_query.filter(
+                Observation.result_time < less_recent,
+            ).order_by(sqlalchemy.desc(Observation.result_time))
+
+            chunks = pd.read_sql(
+                query.statement,
+                query.session.bind,
+                parse_dates=True,
+                index_col="result_time",
+                chunksize=window,
+            )
+            try:
+                window_data = next(chunks)
+            except StopIteration:  # no data
+                window_data = pd.DataFrame([])
+
+        # datetime window
+        # ---------------
+        # get arbitrary many observations, which lies in the
+        # window before the first observation. The window is
+        # defined by a datetime-offset e.g. `'7d'`
+        else:
+            window: datetime.timedelta
+            query = base_query.filter(
+                Observation.result_time < less_recent,
+                Observation.result_time >= less_recent - window,
+            )
+            window_data = pd.read_sql(
+                query.statement,
+                query.session.bind,
+                parse_dates=True,
+                index_col="result_time",
+            )
+
+        if window_data.empty:
+            data = window_data
+        else:
+            data = pd.concat([main_data, window_data], sort=True)
+
+        return data
 
     def run_config(self, datastore: SqlAlchemyDatastore, config):
-        tests = config["tests"]
-        window = config["context_window"]
-        for test in tests:
+        window = self.parse_window(config["context_window"])
+        for test in config["tests"]:
             pos = test["position"]
             function = test["function"]
             kwargs = test["kwargs"]
             stream = self.get_datastream(datastore, pos)
             df = self.get_data(datastore, stream, window)
-            self.run_saqc_funtion(None, function, kwargs)
+            if df is None:
+                continue
+            ctx = dict(stream=stream)
+            self.run_saqc_funtion(None, function, kwargs, ctx)
 
-    def run_saqc_funtion(self, qc_obj, func, kwargs):
+    def run_saqc_funtion(self, qc_obj, func, kwargs, ctx):
         qc = saqc.SaQC()
         method = getattr(qc, func, None)
         if method is None:
-            raise RuntimeError(f"Function {func} not found")
-        print(method)
+            raise RuntimeError(f"Function {func} not found. {ctx=}")
+        ctx['func_name'] = method.__name__
+        logging.debug(f"{ctx=}")
 
     def act(self, message: dict):
         topic = message.get("topic")
