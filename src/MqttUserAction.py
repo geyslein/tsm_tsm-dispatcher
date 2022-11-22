54165a32dd8d13dc2ed08e714225632a1a3fa3f2
Luca Johannes Nendel <luca-johannes.nendel@ufz.de>
2022-11-22 12:46:17 +0100

    Add Schema File Update act() change indentation

--- src/MqttUserAction.py
+++ src/MqttUserAction.py
@@ -11,6 +11,9 @@ from AbstractAction import AbstractAction
 from thing import Thing
 
 class MqttUserAction(AbstractAction):
+
+    SCHEMA_FILE = './avro_schema_files/thing_event.avsc'
+
     def __init__(self, topic, mqtt_broker, mqtt_user, mqtt_password, database_settings: dict):
         super().__init__(topic, mqtt_broker, mqtt_user, mqtt_password)
         self.db = psycopg2.connect(database_settings.get('url'))
@@ -18,20 +21,23 @@ class MqttUserAction(AbstractAction):
     def act(self, message: dict):
         thing = Thing.get_instance(message)
 
+        self.create_user(thing)
+
     def create_user(self, thing):
         get_id = 'SELECT MAX(id) FROM mqtt_auth.mqtt_user;'
         sql = 'INSERT INTO mqtt_user (id,thing_uuid,username,password,description,properties) VALUES (%s, %s, %s, ' \
-              '%s ,%s ,%s )  ON CONFLICT (thing_uuid) DO UPDATE SET name = EXCLUDED.username,' \
+              '%s ,%s ,%s ) ON CONFLICT (thing_uuid) DO UPDATE SET name = EXCLUDED.username,' \
               ' password=EXCLUDED.password' \
               ' description = EXCLUDED.description,' \
               ' properties = EXCLUDED.properties'
-
+              
         with self.db:
             with self.db.cursor() as c:
                 max_id = c.execute(get_id)
                 c.execute(
                     sql,
-                    (max_id+1, thing.uuid, thing.mqtt_user, thing.password, thing.description, json.dumps(thing.properties))
+                    (max_id+1, thing.uuid, "test", "test", thing.description,
+                     json.dumps(thing.properties))
                 )
 
 
