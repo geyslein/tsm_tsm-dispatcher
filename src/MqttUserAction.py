c2c42e4a00bf0d7ce401a245d86f246eceb7ad05
Luca Johannes Nendel <luca-johannes.nendel@ufz.de>
2022-11-23 14:27:47 +0100

    add check for mqtt_authentication_credentials

--- src/MqttUserAction.py
+++ src/MqttUserAction.py
@@ -1,10 +1,6 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import annotations
-
 import json
 import psycopg2
+import json
 from psycopg2 import sql as psysql
 
 from AbstractAction import AbstractAction
@@ -17,32 +13,31 @@ class MqttUserAction(AbstractAction):
     def __init__(self, topic, mqtt_broker, mqtt_user, mqtt_password, database_settings: dict):
         super().__init__(topic, mqtt_broker, mqtt_user, mqtt_password)
         self.db = psycopg2.connect(database_settings.get('url'))
+        print("innit m8")
 
     def act(self, message: dict):
         thing = Thing.get_instance(message)
+        if message['mqtt_authentication_credentials']:
+            user = message['mqtt_authentication_credentials'][0]
+            pw = message['mqtt_authentication_credentials'][1]
+            print(user)
+            print(pw)
+            self.create_user(thing, user, pw)
 
-        self.create_user(thing)
-
-    def create_user(self, thing):
+    def create_user(self, thing, user, pw):
         get_id = 'SELECT MAX(id) FROM mqtt_auth.mqtt_user;'
         sql = 'INSERT INTO mqtt_user (id,thing_uuid,username,password,description,properties) VALUES (%s, %s, %s, ' \
               '%s ,%s ,%s ) ON CONFLICT (thing_uuid) DO UPDATE SET name = EXCLUDED.username,' \
               ' password=EXCLUDED.password' \
               ' description = EXCLUDED.description,' \
               ' properties = EXCLUDED.properties'
-              
+        print("db stuff")
         with self.db:
             with self.db.cursor() as c:
                 max_id = c.execute(get_id)
                 c.execute(
                     sql,
-                    (max_id+1, thing.uuid, "test", "test", thing.description,
+                    (max_id+1, thing.uuid, user, pw, thing.description,
                      json.dumps(thing.properties))
                 )
-
-
-
-
-
-
 
