8bbcf733586e873e24bd7c1ae4562b163186b519
Bert Palm <bert.palm@ufz.de>
2023-01-25 13:53:37 +0100

    on_message Exception fix. log Exception but do not die.

--- src/MqttHelper.py
+++ src/MqttHelper.py
@@ -1,38 +1,48 @@
 import logging
 import json
+import os.path
+
 from AvroSchemaValidator import validate_avro_schema
 from fastavro._validate_common import ValidationError
 
 
 def on_message(client, userdata, message):
-    logging.info(f"Received message on topic '{message.topic}' with QoS {message.qos}")
+    logging.info(
+        f"received message {message.mid} "
+        f"on topic '{message.topic}' with QoS {message.qos}"
+    )
+    logging.debug(f"{message=}")
 
     callback = userdata["act"]
     schema_file = userdata["schema_file"]
-    try:
-        payload = str(message.payload.decode("utf-8"))
-        content = json.loads(payload)
-    except json.JSONDecodeError as e:
-        logging.error(f"message parsing failed with: {type(e).__name__}: {e}")
-        return
-
-    if schema_file is None:
-        pass
-    elif validate_avro_schema(content, schema_file):
-        # must be an avro proofed messages, otherwise we
-        # might overwrite a user-set 'topic'
-        content["topic"] = message.topic
-    else:
-        logging.error("avro schema validation failed: discarding message")
-        return
 
     try:
+        decoded: str = message.payload.decode("utf-8")
+
+        if schema_file is None:
+            try:
+                # also parse single numeric values
+                # and the constants null, +/-Infinity, NoN
+                content = json.loads(decoded)
+            except json.JSONDecodeError:
+                # string / datetime / other
+                content = decoded
+
+        # we have an avro schema to check against
+        else:
+            name = os.path.basename(schema_file)
+            content = json.loads(decoded)
+            if not validate_avro_schema(content, schema_file):
+                raise ValueError(f"Received message does not match avro schema '{name}'")
+            logging.debug(f"Received message matches avro schema '{name}'")
+
+        if isinstance(content, dict):
+            content["topic"] = message.topic
+
         callback(content)
+
     except Exception as e:
-        logging.error(
-            f"message processing failed with: {type(e).__name__}:{e}\n"
-            f"original message: {payload}"
-        )
+        logging.error(f"Errors occurred. discarding message {message.mid}", exc_info=e)
 
 
 def on_log(client, userdata, level, buf):
